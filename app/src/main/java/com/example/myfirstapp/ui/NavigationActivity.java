ac6a447ee8412f9f64d13d05e89b4f8bd6354be5
XY <wxy888@live.com>
2017-03-31 11:21:02 +0800

    fragments for expenditure and budget

--- app/src/main/java/com/example/myfirstapp/ui/NavigationActivity.java
+++ app/src/main/java/com/example/myfirstapp/ui/NavigationActivity.java
@@ -9,20 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.classes.Expenditure;
 
 public class NavigationActivity extends AppCompatActivity {
 
-
+    Fragment currentFragment = null;
     Fragment selectedFragment = null;
     Fragment Cart = CartFragment.newInstance();
     boolean firstExpenditure = true;
     boolean firstHistory = true;
+
+
     String tag;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout);
             switch (item.getItemId()) {
                 case R.id.menu_grocery_list:
                     selectedFragment = getSupportFragmentManager().findFragmentByTag("Cart");
@@ -54,6 +58,7 @@ public class NavigationActivity extends AppCompatActivity {
                     break;
             }
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            //transaction.hide(currentFragment);
             transaction.replace(R.id.frame_layout, selectedFragment, tag);
             transaction.addToBackStack(null);
             transaction.commit();
@@ -70,19 +75,38 @@ public class NavigationActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
         transaction.replace(R.id.frame_layout, Cart, "Cart");
         transaction.commit();
     }
 
     public void replaceGrocery(){
+
+        selectedFragment = SelectCategoryFragment.newInstance();
+        currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        Fragment selectedFragment = SelectCategoryFragment.newInstance();
-        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout);
+
         transaction.add(R.id.frame_layout, selectedFragment, "Cart");
-        transaction.hide(currentFragment);
         transaction.replace(R.id.frame_layout, selectedFragment);
         transaction.commit();
     }
 
+    public void replaceExpenditure(){
+        selectedFragment = BudgetFragment.newInstance();
+        currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.add(R.id.frame_layout, selectedFragment, "Expenditure");
+        transaction.replace(R.id.frame_layout, selectedFragment);
+        transaction.commit();
+    }
+
+    public void replaceBudget(){
+        selectedFragment = ExpenditureFragment.newInstance();
+        currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame_layout);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.add(R.id.frame_layout, selectedFragment, "Expenditure");
+        transaction.replace(R.id.frame_layout, selectedFragment);
+        transaction.commit();
+    }
 }
