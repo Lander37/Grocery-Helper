78cb7c521d43464625060fee969b357b1e933d6d
tosytan <TTAN032@e.ntu.edu.sg>
2017-04-03 23:14:18 +0800

    changed comment in one grocery manager to supermarket manager

--- app/src/main/java/com/example/myfirstapp/mgr/SupermarketManager.java
+++ app/src/main/java/com/example/myfirstapp/mgr/SupermarketManager.java
@@ -47,8 +47,10 @@ public class SupermarketManager {
     }
 
     /**
-     *
-     * @return
+     * This method retrieves the list of products that
+     * are available in the current supermarket
+     * with the same type/name.
+     * @return List of products.
      */
     //Get Full Product List of Current Supermarket
     public Product[] findSMProductList(){
