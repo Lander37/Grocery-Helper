3bfe759ab55b6b1e2137f7e8e6339a8a0a46e81e
XY <wxy888@live.com>
2017-04-05 01:16:50 +0800

    visual adjustments

--- app/src/main/java/com/example/myfirstapp/ui/SelectBrandFragment.java
+++ app/src/main/java/com/example/myfirstapp/ui/SelectBrandFragment.java
@@ -7,9 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.dbHelpers.DatabaseAccess;
+
+import java.util.List;
 
 public class SelectBrandFragment extends Fragment {
 
@@ -17,13 +22,18 @@ public class SelectBrandFragment extends Fragment {
 
     Button btBack;
     Button btDelete;
+    ListView lvBrandsList;
+    DatabaseAccess databaseAccess;
     private int gl_id;
+    private String subCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle args = getArguments();
         gl_id = args.getInt("gl_id");
+        subCategory =args.getString("subCategory");
+
     }
 
     @Override
@@ -33,6 +43,9 @@ public class SelectBrandFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_select_brand, container, false);
         btBack = (Button) view.findViewById(R.id.backB);
         btDelete= (Button) view.findViewById(R.id.deleteB);
+        this.databaseAccess = DatabaseAccess.getInstance(getActivity().getApplicationContext());
+        lvBrandsList = (ListView) view.findViewById(R.id.list);
+
         btBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,18 +56,27 @@ public class SelectBrandFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 /*Code for deleting this item from database
-                ?
-                 */
+                  refer to gl_id to find right list, and subCategory to find right item.
+                */
                 ((NavigationActivity) getActivity()).replaceThis(SpecificListFragment.newInstance(gl_id), "Cart");
             }
         });
+
+        databaseAccess.open();
+        /*Code for getting a list of brands
+        List<String> prodList = databaseAccess.getBrands(subCategory); <-implement getBrands method
+        */
+        databaseAccess.close();
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, prodList);
+        //this.lvBrandsList.setAdapter(adapter);
         return view;
     }
 
-    public static SelectBrandFragment newInstance(int gl_id) {
+    public static SelectBrandFragment newInstance(int gl_id, String subCategory) {
         SelectBrandFragment fragment = new SelectBrandFragment();
         Bundle args = new Bundle();
         args.putInt("gl_id",gl_id);
+        args.putString("subCategory",subCategory);
         fragment.setArguments(args);
         return fragment;
     }
